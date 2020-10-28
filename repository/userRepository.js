6aa260fcbb6c216acba44f404e0a1bab70724e13
JoaoCCM <joaocarlosc.m@hotmail.com>
2020-10-27 21:52:34 -0300

    encrypting password

--- repository/userRepository.js
+++ repository/userRepository.js
@@ -1,18 +1,18 @@
 module.exports = (app) => {
-  const createUser = async (data) => {
-    try {
-      const { nome, email, senha, foto, curso } = data;
-      const query = await app.db
-        .raw(
-          `CREATE (a:Aluno { nome:'${nome}', email: '${email}', senha: '${senha}', foto: '${foto}', curso: '${curso}'})`
-        )
-        .run();
+    const createUser = async ({ nome, email, senha, foto, curso }) => {
+        try {
+            // const { nome, email, senha, foto, curso } = data;
+            const query = await app.db
+                .raw(
+                    `CREATE (a:Aluno { nome:'${nome}', email: '${email}', senha: '${senha}', foto: '${foto}', curso: '${curso}'})`
+                )
+                .run();
 
-      return { id: query[0] };
-    } catch (e) {
-      throw e;
-    }
-  };
+            return { id: query[0] };
+        } catch (e) {
+            throw e;
+        }
+    };
 
-  return { createUser };
+    return { createUser };
 };
--- services/user.js
+++ services/user.js
@@ -1,14 +1,31 @@
+const bcrypt = require("bcrypt");
+
 module.exports = (app) => {
-  const { createUser } = app.repository.userRepository;
+    const { createUser } = app.repository.userRepository;
 
-  const createOne = async (data) => {
-    try {
-      const user = await createUser(data);
-      return user;
-    } catch (error) {
-      throw error;
-    }
-  };
+    const createOne = async (data) => {
+        try {
+            const { nome, email, senha, foto, curso } = data;
+            const hashed = await generateHash(senha);
+            const user = await createUser({
+                nome,
+                email,
+                senha: hashed,
+                foto,
+                curso,
+            });
+            return user;
+        } catch (error) {
+            throw error;
+        }
+    };
 
-  return { createOne };
+    const generateHash = async (senha) => {
+        const hash = await bcrypt.hash(senha, 10);
+        const hashedSenha = hash;
+
+        return hashedSenha;
+    };
+
+    return { createOne };
 };
