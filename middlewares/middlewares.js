0544e61e39c646b36a9df083f8b599a8d5aa849e
JoaoCCM <joaocarlosc.m@hotmail.com>
2020-11-03 10:18:29 -0300

    add swagger

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -2,6 +2,39 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const logger = require("morgan");
+const swaggerJsdocs = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.1',
+        info: {
+            // swagger: "2.0",
+            title: 'Connect<IF>',
+            // version: "1.0.0",
+            description: 'Connect<IF> API information',
+
+            servers: [`http://localhost:5000`],
+        },
+        // components: {
+        //     securitySchemes: {
+        //         bearerAuth: {
+        //             type: 'http',
+        //             scheme: 'bearer',
+        //             bearerFormat: 'JWT',
+        //         },
+        //     },
+        // },
+        // security: [
+        //     {
+        //         bearerAuth: [],
+        //     },
+        // ],
+    },
+    apis: ['./routes/*.js'],
+}
+const swaggerDocs = swaggerJsdocs(swaggerOptions)
+
 
 module.exports = app => {
     app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,4 +42,5 @@ module.exports = app => {
     app.use(cors());
     app.use(helmet());
     app.use(logger("dev"));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,43 @@
 module.exports = (app) => {
+    /**
+     * @swagger
+     * components:
+     *   schemas:
+     *     User:
+     *       type: object
+     *       properties:
+     *          email:
+     *            type: string
+     *          nome:
+     *            type: string
+     *          senha:
+     *            type: string
+     *          curso:
+     *            type: string
+     *
+     *
+     *
+     * @swagger
+     * /user:
+     *     post:
+     *        tags:
+     *          - user
+     *        description: Use to register a user
+     *        produces:
+     *          - application/json
+     *        requestBody:
+     *          content:
+     *            application/json:
+     *              schema:
+     *                $ref: '#/components/schemas/User'
+     *        responses:
+     *          '200':
+     *            description: Created
+     *            schema:
+     *              $ref: '#/components/schemas/User'
+     *
+     */
+
     const { create, find } = app.controllers.userController
 
     app.post('/user', create)
